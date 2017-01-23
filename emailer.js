1a17c4eb03c2c216b2e5767073b96982cebd3b2f
Thomas Dailey <daileytj89@gmail.com>
2017-01-23 18:32:44 -0500

    email alert middleware solution

---
+++ emailer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+const {logger} = require('./utilities/logger');
+
+// stored in `.env` -- never store passwords, api keys
+// etc. inside source code
+const {SMTP_URL} = process.env;
+
+
+// `emailData` is an object that looks like this:
+// {
+//  from: "foo@bar.com",
+//  to: "bizz@bang.com, marco@polo.com",
+//  subject: "Hello world",
+//  text: "Plain text content",
+//  html: "<p>HTML version</p>"
+// }
+const sendEmail = (emailData, smtpUrl=SMTP_URL) => {
+  const transporter = nodemailer.createTransport(SMTP_URL);
+  console.log("send email function ran");
+  logger.info(`Attempting to send email from ${emailData.from}`);
+  return transporter
+    .sendMail(emailData)
+    .then(info => console.log(`Message sent: ${info.response}`))
+    .catch(err => console.log(`Problem sending email: ${err}`));
+};
+
+//console.log("emailer js running");
+module.exports = {sendEmail};
